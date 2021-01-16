9d234464498e6deb255938aab63b19866f3c39db
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-01-16 19:14:24 +0700

    Revert "Backend FrontPage MemberPage Setting"
    This reverts commit 2c8f02d0f130d5c89f897873f8ca8d01547935fe.

--- backend/api-gateway/routes/refreshTokens.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const {APP_NAME} = process.env;
-/* GET users listing. */
-
-const refreshTokensHandler = require('./handler/refresh-tokens')
-router.post('/', refreshTokensHandler.refreshToken);
-
-module.exports = router;
--- backend/service-user/routes/refreshToken.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const refreshTokenHandler = require('./handler/refreshToken');
-
-router.post('/', refreshTokenHandler.create);
-router.get('/', refreshTokenHandler.getToken);
-
-module.exports = router;
