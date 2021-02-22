a68dd60ad2a843bb2668d0c231b153e300d62d53
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-02-23 00:04:17 +0700

    banyak API sampe api gateway 4

--- frontpage/.next/static/runtime/webpack.js
+++ frontpage/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c187319e7632a89d9d40";
+/******/ 	var hotCurrentHash = "97d0cccf90b5089fdc2b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
