c4f38190a1702633246e8ef775e2f4e64e448da2
Ryan Hartadi <Ryanhartadi06@gmail.com>
2021-05-06 16:59:18 +0700

    Changed

--- backend/service-user/routes/refreshToken.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const refreshTokenHandler = require("./handler/refresh-token");
-/* GET users listing. */
-router.post("/", refreshTokenHandler.create);
-router.get("/", refreshTokenHandler.getToken);
-
-module.exports = router;
